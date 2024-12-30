b809798e861e5777b78954d900b0ca8876e92d7f
CJ Williams <cj@cjwilliams.io>
2024-12-30 18:08:36 +0000

    Add page view counter

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), db()]
 });
