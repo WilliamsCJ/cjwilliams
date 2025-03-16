3b13ff69117369bec08ab53114e07f80ff7f9f45
CJ Williams <cj@cjwilliams.io>
2025-03-16 16:05:08 +0000

    Handle trailing slash

--- apps/website/src/utils.ts
+++ apps/website/src/utils.ts
@@ -1,3 +1,15 @@
 export function prepareTagsForDisplay(tags: string[]): string {
   return tags.map((t: string) => `#${t}`).join(" ")
 }
+
+export function removeTrailingSlash(path: string): string {
+  if (path === "/") {
+    return path
+  }
+
+  if (path.slice(-1) === "/") {
+    return path.slice(0, -1)
+  }
+
+  return path
+}
